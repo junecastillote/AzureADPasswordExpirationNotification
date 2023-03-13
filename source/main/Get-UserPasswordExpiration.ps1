4fa552c299da16ac39b0b1e3efeedce769a2ca1c
June Castillote <june.castillote@gmail.com>
2023-03-13 13:25:09 +0800

    Update Get-UserPasswordExpiration.ps1

--- source/main/Get-UserPasswordExpiration.ps1
+++ source/main/Get-UserPasswordExpiration.ps1
@@ -38,30 +38,9 @@ Function Get-UserPasswordExpiration {
     ## Retrieve the access token from the session
     $AccessToken = (Get-AccessToken).access_token
 
-    ## Get enabled accounts excluding guests
-    try {
-        SayInfo "Getting all enabled accounts excluding guests."
-        $uri = "https://graph.microsoft.com/beta/users?`$filter=userType eq 'member' and accountEnabled eq true&`$select=UserPrincipalName,mail,displayName,PasswordPolicies,LastPasswordChangeDateTime&`$top=999&`$count=true"
-        $result = (Invoke-RestMethod -Method Get -Uri $uri -Headers @{Authorization = "Bearer $AccessToken"; ConsistencyLevel = 'Eventual' } -ContentType 'application/json' -ErrorAction Stop)
-        $users = [System.Collections.ArrayList]@()
-        $users.AddRange($($result.value | Where-Object { $_.PasswordPolicies -ne 'DisablePasswordExpiration' } ))
-        $totalUserCount = $result.'@odata.count'
-        SayInfo "$($users.Count) of $($totalUserCount) users"
-        while ($result.'@odata.nextLink') {
-            $result = (Invoke-RestMethod -Method Get -Uri $result.'@odata.nextLink' -Headers @{Authorization = "Bearer $AccessToken" } -ContentType 'application/json' -ErrorAction Stop)
-            $users.AddRange($($result.value | Where-Object { $_.PasswordPolicies -ne 'DisablePasswordExpiration' } ))
-            SayInfo "$($users.Count) of $($totalUserCount) users"
-        }
-    }
-    catch {
-        SayError 'There was an error getting the list of users in Azure AD. The script will terminate.'
-        SayError $_.Exception.Message
-        return $null
-    }
-
     ## Get domains list
     try {
-        SayInfo "Getting all domains."
+        SayInfo "Getting all domains password policies."
         $uri = "https://graph.microsoft.com/v1.0/domains"
         $domains = [System.Collections.ArrayList]@($(Invoke-RestMethod -Method Get -Uri $uri -Headers @{Authorization = "Bearer $AccessToken" } -ContentType 'application/json' -ErrorAction Stop).Value) | Sort-Object Id
         foreach ($domain in $domains) {
@@ -88,6 +67,27 @@ Function Get-UserPasswordExpiration {
     $domainTable = [ordered]@{}
     foreach ($item in $domains.SyncRoot) {
         $domainTable.Add($item.id, $item.passwordValidityPeriodInDays)
+    }
+
+    ## Get enabled accounts excluding guests
+    try {
+        SayInfo "Getting all enabled accounts excluding guests and with DisablePasswordExpiration policy assigned."
+        $uri = "https://graph.microsoft.com/beta/users?`$filter=userType eq 'member' and accountEnabled eq true&`$select=UserPrincipalName,mail,displayName,PasswordPolicies,LastPasswordChangeDateTime&`$top=999&`$count=true"
+        $result = (Invoke-RestMethod -Method Get -Uri $uri -Headers @{Authorization = "Bearer $AccessToken"; ConsistencyLevel = 'Eventual' } -ContentType 'application/json' -ErrorAction Stop)
+        $users = [System.Collections.ArrayList]@()
+        $users.AddRange($($result.value | Where-Object { $_.PasswordPolicies -ne 'DisablePasswordExpiration' } ))
+        $totalUserCount = $result.'@odata.count'
+        SayInfo "$($users.Count) of $($totalUserCount) users"
+        while ($result.'@odata.nextLink') {
+            $result = (Invoke-RestMethod -Method Get -Uri $result.'@odata.nextLink' -Headers @{Authorization = "Bearer $AccessToken" } -ContentType 'application/json' -ErrorAction Stop)
+            $users.AddRange($($result.value | Where-Object { $_.PasswordPolicies -ne 'DisablePasswordExpiration' } ))
+            SayInfo "$($users.Count) of $($totalUserCount) users"
+        }
+    }
+    catch {
+        SayError 'There was an error getting the list of users in Azure AD. The script will terminate.'
+        SayError $_.Exception.Message
+        return $null
     }
 
     ## Add properties to the $users objects
